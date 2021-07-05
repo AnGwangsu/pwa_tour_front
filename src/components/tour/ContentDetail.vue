f197ab0d2a13cdaa1a886ac01e9420ba6945c621
안광수 <jhk7716@gmail.com>
2021-07-05 17:26:01 +0900

    임시저장

--- src/components/tour/ContentDetail.vue
+++ src/components/tour/ContentDetail.vue
@@ -2,7 +2,18 @@
     <v-container fluid style="padding:0">
         <v-layout>
             <v-flex>
-
+                <v-layout class="pa-3">
+                    <v-flex><v-icon size="30" @click="moveBack">mdi-chevron-left</v-icon></v-flex>
+                </v-layout>
+                <v-flex class="mt-3">
+                    <v-carousel hide-delimiters height="250">
+                        <v-carousel-item 
+                        v-for="(img,index) in image" 
+                        :key="index" 
+                        :src="img">
+                        </v-carousel-item>
+                    </v-carousel>
+                </v-flex>
             </v-flex>
         </v-layout>
     </v-container>
@@ -28,6 +39,11 @@ export default {
         this.dataDetail()
     },
     methods:{
+        moveBack(){
+            this.$router.push({
+                path:'/home'
+            })
+        },
         async dataDetail(){
             try {
                 var res = await this.axios.post('/data/read',{
@@ -37,7 +53,7 @@ export default {
                 if(res.data.resultCode==1){
                     var data = res.data.data
                     this.image=data.image
-                    this.info=data.intro.items.item
+                    this.info=data.info
 
                     console.log(this.image)
                     console.log(this.info)
